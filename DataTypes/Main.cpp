3ee99ec69c5de6e706a2f3bb2781f8d6d5624d44
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-12-01 08:34:41 +0000

    while and for loop

--- DataTypes/Main.cpp
+++ DataTypes/Main.cpp
@@ -3,6 +3,14 @@
 int main() {
     // Write C++ code here
     std::cout << "Hello wolrd";
+    for(int i = 0; i<=10; i++){
+        std::cout << i << "\n";
+    }
 
+    int j = 10;
+    while(j >=1 ){
+        std::cout << j << '\n';
+        j--;
+    }
     return 0;
 }
